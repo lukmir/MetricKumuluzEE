65d07431cd8c3c724173ec39f79dcdf6f90509a0
lukmir <lukmir2@gmail.com>
2018-04-08 15:07:55 +0200

    adding db support

--- src/main/java/com/kumuluzee/metric/util/db/Database.java
+++ src/main/java/com/kumuluzee/metric/util/db/Database.java
@@ -1,4 +1,28 @@
 package com.kumuluzee.metric.util.db;
 
+import com.kumuluzee.metric.model.rest.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database {
+
+  public static List<Customer> customerList = new ArrayList<>();
+
+  public static List<Customer> getCustomers() {
+    return customerList;
+  }
+
+  public static void addCustomer(Customer customer) {
+    customerList.add(customer);
+  }
+
+  public static void deleteCustomer(String customerId) {
+    for (Customer customer : customerList) {
+      if (customer.getId().equals(customerId)) {
+        customerList.remove(customer);
+        break;
+      }
+    }
+  }
 }
