2281724eced076b0646bd79cd1f022b86977a6f3
lukmir <lukmir2@gmail.com>
2018-04-08 14:26:04 +0200

    initial commit

---
+++ src/main/java/com/kumuluzee/metric/controller/rest/CustomerApplication.java
@@ -0,0 +1,8 @@
+package com.kumuluzee.metric.controller.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class CustomerApplication extends Application {
+}
